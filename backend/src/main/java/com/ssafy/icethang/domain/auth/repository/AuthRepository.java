4aa2395887e92e2c0f1ba8389882fc75414b3076
sangji <139340193+theundergroundt@users.noreply.github.com>
2026-01-19 17:56:09 +0900

    [S14P11E204-29] feature : auth 엔티티 및 레포지토리 구현

---
+++ backend/src/main/java/com/ssafy/icethang/domain/auth/repository/AuthRepository.java
@@ -0,0 +1,12 @@
+package com.ssafy.icethang.domain.auth.repository;
+
+import com.ssafy.icethang.domain.auth.entity.Auth;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthRepository extends JpaRepository<Auth, String> {
+    Optional<Auth> findByEmail(String email);
+}
