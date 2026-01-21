c9309efd7c9955a1625343b4523a8df402f6fbba
김상지 <139340193+theundergroundt@users.noreply.github.com>
2026-01-21 12:36:32 +0900

    [S14P11E204-52] feature : 토큰 dto 구현

---
+++ backend/src/main/java/com/ssafy/icethang/domain/auth/dto/response/TokenResponseDto.java
@@ -0,0 +1,15 @@
+package com.ssafy.icethang.domain.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenResponseDto {
+    private String accessToken;
+    private String refreshToken;
+}
