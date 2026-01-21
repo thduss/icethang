013e995c2ce554ca69e71926599cc0633483396c
김상지 <139340193+theundergroundt@users.noreply.github.com>
2026-01-21 17:37:01 +0900

    [S14P11E204-56] feature : 쿠키 유틸 메서드 구현 및 역,직렬화 구현

--- backend/src/main/java/com/ssafy/icethang/global/utill/CookieUtil.java
+++ backend/src/main/java/com/ssafy/icethang/global/utill/CookieUtil.java
@@ -1,8 +1,14 @@
 package com.ssafy.icethang.global.utill;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
+import org.springframework.util.SerializationUtils;
+
+import java.util.Base64;
+import java.util.Optional;
 
 @Component
 public class CookieUtil {
@@ -63,5 +69,54 @@ public class CookieUtil {
 
         response.addHeader("Set-Cookie", accessCookie.toString());
         response.addHeader("Set-Cookie", refreshCookie.toString());
+    }
+
+    public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(name)) {
+                    return Optional.of(cookie);
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
+    // 2. 일반 쿠키 저장 (OAuth2 요청 정보 저장용)
+    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
+
+    // 3. 일반 쿠키 삭제 (이름으로 삭제)
+    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(name)) {
+                    cookie.setValue("");
+                    cookie.setPath("/");
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                }
+            }
+        }
+    }
+
+    // 직렬화
+    // 4. 객체 -> 문자열
+    public static String serialize(Object object) {
+        return Base64.getUrlEncoder()
+                .encodeToString(SerializationUtils.serialize(object));
+    }
+
+    // 5. 문자열 -> 객체 변환
+    public static <T> T deserialize(Cookie cookie, Class<T> cls) {
+        return cls.cast(SerializationUtils.deserialize(
+                Base64.getUrlDecoder().decode(cookie.getValue())));
     }
 }
