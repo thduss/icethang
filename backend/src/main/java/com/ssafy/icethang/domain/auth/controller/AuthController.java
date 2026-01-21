95f5f3f6ec50958869346b6ae444640023e29cb4
김상지 <139340193+theundergroundt@users.noreply.github.com>
2026-01-21 12:37:55 +0900

    [S14P11E204-52] feature : 토큰 재발급 api 구현

--- backend/src/main/java/com/ssafy/icethang/domain/auth/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/icethang/domain/auth/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.ssafy.icethang.domain.auth.controller;
 import com.ssafy.icethang.domain.auth.dto.request.LoginRequest;
 import com.ssafy.icethang.domain.auth.dto.request.SignupRequest;
 import com.ssafy.icethang.domain.auth.dto.request.UpdateUserRequest;
+import com.ssafy.icethang.domain.auth.dto.response.TokenRequestDto;
+import com.ssafy.icethang.domain.auth.dto.response.TokenResponseDto;
 import com.ssafy.icethang.domain.auth.entity.Auth;
 import com.ssafy.icethang.domain.auth.service.AuthService;
 import com.ssafy.icethang.global.security.UserPrincipal;
@@ -39,24 +41,31 @@ public class AuthController {
         authService.updateUser(userPrincipal.getEmail(), request);
         return ResponseEntity.ok("회원 정보가 수정되었습니다.");
     }
-    // 로그아웃
-    @PostMapping("/logout")
-    public ResponseEntity<String> logout() {
-        // JWT는 원래 서버에서 할 게 없다(?)
-        // 클라이언트가 자기 폰에서 토큰 지우면 그게 로그아웃
-        // 나중에 리프레시 토큰 생기면 여기서 DB의 리프레시 토큰을 지워야 한다(?)
-        return ResponseEntity.ok("로그아웃 되었습니다. (프론트에서 토큰을 삭제하세요)");
-    }
 
     // 학생 로그인
 
     // 선생님 로그인
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
-        String token = authService.login(request);
+    public ResponseEntity<TokenResponseDto> login(@RequestBody LoginRequest request) {
+
         // 토큰을 헤더에 넣을지 바디에 넣을지는 프론트랑 합의 (보통 바디나 헤더)
-        return ResponseEntity.ok(token);
+        return ResponseEntity.ok(authService.login(request));
     }
 
     // 토큰 재발급
+    @PostMapping("/refresh")
+    public ResponseEntity<TokenResponseDto> reissue(@RequestBody TokenRequestDto request){
+        return ResponseEntity.ok(authService.reissue(request));
+    }
+
+    // 로그아웃
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@RequestHeader("Authorization") String accessToken,
+                                         @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        // bearer 떼기
+        String token = accessToken.substring(7);
+
+        authService.logout(token, userPrincipal.getEmail());
+        return ResponseEntity.ok("로그아웃 되었습니다.");
+    }
 }
