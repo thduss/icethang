ae629381330568c912d52167e4580a498e79affc
김상지 <139340193+theundergroundt@users.noreply.github.com>
2026-01-21 17:37:56 +0900

    [S14P11E204-56] feature : Redirect 로직 구현

--- backend/src/main/java/com/ssafy/icethang/global/security/OAuth2SuccessHandler.java
+++ backend/src/main/java/com/ssafy/icethang/global/security/OAuth2SuccessHandler.java
@@ -1,8 +1,10 @@
 package com.ssafy.icethang.global.security;
 
 import com.ssafy.icethang.global.redis.RedisService;
+import com.ssafy.icethang.global.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository;
 import com.ssafy.icethang.global.utill.CookieUtil;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,9 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Optional;
+
+import static com.ssafy.icethang.global.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
 
 @Component
 @RequiredArgsConstructor
@@ -20,6 +25,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private final TokenProvider tokenProvider;
     private final RedisService redisService;
     private final CookieUtil cookieUtil;
+    private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -40,14 +46,31 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         // 4. 리다이렉트 설정
         // 나중에 프론트엔드 주소로 설정
         // 아마도 리액트 3000번일듯
-        String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:8080/")
-                .build().toUriString();
+        String targetUrl = determineTargetUrl(request, response, authentication);
 
         if (response.isCommitted()) {
             return;
         }
 
+        clearAuthenticationAttributes(request, response);
         // 4. 프론트엔드로 리다이렉트
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+
+    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        // 쿠키에서 redirect_uri 찾기
+        Optional<String> redirectUri = CookieUtil.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue);
+
+        // 나중에 프론트엔드 주소로 바꿔야 함
+        String targetUrl = redirectUri.orElse("http://localhost:8080/");
+
+        return UriComponentsBuilder.fromUriString(targetUrl)
+                .build().toUriString();
+    }
+
+    protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+        httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
     }
 }
