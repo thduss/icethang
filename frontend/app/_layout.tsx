cbd57cd8c5d20d5d528e2af53c9ac8379cfcc1e5
박근영 <rmsdud951226@naver.com>
2026-01-23 16:06:46 +0900

    [S14P11E204-71] feature : 학생 입장하기 페이지 구현

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -4,14 +4,11 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
 import { View } from 'react-native';
-
-// 스플래시 화면 제어
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
 
   useEffect(() => {
-    // 앱이 켜지고 0.5초 뒤에 스플래시 화면 숨기기 (안전장치)
     const timer = setTimeout(async () => {
       await SplashScreen.hideAsync();
     }, 500);
@@ -20,25 +17,14 @@ export default function RootLayout() {
   }, []);
 
   return (
-    // 🚨 중요: 여기에 <View>나 <TeacherLoginScreen />을 직접 넣으면 안 됩니다!
-    // 무조건 <Stack>이 가장 바깥에 있어야 'Navigation Context' 에러가 안 납니다.
     <ThemeProvider>
       <Stack screenOptions={{ headerShown: false }}>
-        
-        {/* 1. 메인 화면 (index.tsx) */}
         <Stack.Screen name="index" />
-
-        {/* 2. 선택 화면 */}
         <Stack.Screen name="screens/select/index" />
-
-        {/* 3. 교사 로그인 화면 */}
         <Stack.Screen name="screens/teacher_login/index" />
-
-        {/* 4. 학생 로그인 화면 (만약 있다면) */}
         <Stack.Screen name="screens/student_login/index" />
-
         <Stack.Screen name="screens/Teacher_MainPage/TeacherMainPage" />
-        
+
       </Stack>
     </ThemeProvider>
   );
