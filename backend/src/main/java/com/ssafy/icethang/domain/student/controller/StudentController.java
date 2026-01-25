a39544b154819d1674cba8ba08fcf9664c09b14a
theundergroundt <139340193+theundergroundt@users.noreply.github.com>
2026-01-25 23:40:24 +0900

    [S14P11E204-90] feature : student 재로그인로직 AuthController로 통합 및 쿠키 설정 적용

--- backend/src/main/java/com/ssafy/icethang/domain/student/controller/StudentController.java
+++ backend/src/main/java/com/ssafy/icethang/domain/student/controller/StudentController.java
@@ -1,24 +1,13 @@
 package com.ssafy.icethang.domain.student.controller;
 
-import com.ssafy.icethang.domain.student.dto.request.StudentJoinRequest;
-import com.ssafy.icethang.domain.student.dto.response.StudentLoginResponse;
-import com.ssafy.icethang.domain.student.service.StudentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/students")
+@RequestMapping("/students")
 @RequiredArgsConstructor
 public class StudentController {
-    private final StudentService studentService;
 
-    // 학생 최초 가입
-    @PostMapping("/join")
-    public ResponseEntity<StudentLoginResponse> join(@RequestBody StudentJoinRequest request) {
-        return ResponseEntity.ok(studentService.join(request));
-    }
 }
