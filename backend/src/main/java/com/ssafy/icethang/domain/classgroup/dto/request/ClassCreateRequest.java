508e66973ae4ea84a6f1885492148e346d43f9a5
theundergroundt <139340193+theundergroundt@users.noreply.github.com>
2026-01-25 15:29:46 +0900

    [S14P11E204-77] feature : classgroup dto 생성

---
+++ backend/src/main/java/com/ssafy/icethang/domain/classgroup/dto/request/ClassCreateRequest.java
@@ -0,0 +1,10 @@
+package com.ssafy.icethang.domain.classgroup.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ClassCreateRequest {
+    private String groupName;
+}
