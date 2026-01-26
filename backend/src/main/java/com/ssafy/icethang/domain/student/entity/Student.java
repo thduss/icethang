10d49af2176219fc8afc1377fc8722b749137634
김상지 <139340193+theundergroundt@users.noreply.github.com>
2026-01-26 11:10:13 +0900

    [S14P11E204-95] feature : student 엔티티에 수정 메서드 추가 및 수정, 조회 dto 구현

--- backend/src/main/java/com/ssafy/icethang/domain/student/entity/Student.java
+++ backend/src/main/java/com/ssafy/icethang/domain/student/entity/Student.java
@@ -46,4 +46,10 @@ public class Student {
         this.currentXp = 0;
         this.currentLevel = 1;
     }
+
+    // 학생 수정
+    public void updateInfo(String name, Integer studentNumber) {
+        if (name != null) this.name = name;
+        if (studentNumber != null) this.studentNumber = studentNumber;
+    }
 }
