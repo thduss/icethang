7f8d16e597f9ce4d42086f6b5eaf0fd76c9d9ff7
김세현 <sshyeoniz@naver.com>
2026-01-27 10:56:52 +0900

    [S14P11E204-111] refactor : 학급 상태 구조 정의 및 학급 관리 UI 연동

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler'
 import { ThemeProvider } from './context/ThemeContext';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -6,6 +7,7 @@ import { store, persistor } from './store/stores';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ActivityIndicator, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 
 SplashScreen.preventAutoHideAsync();
@@ -21,28 +23,30 @@ export default function RootLayout() {
   }, []);
 
   return (
-    <Provider store={store}>
-    <PersistGate loading={<LoadingScreen />} persistor={persistor}>
-    <ThemeProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="screens/select/index" />
-        <Stack.Screen name="screens/teacher_login/index" />
-        <Stack.Screen name="screens/student_login/index" />
-        <Stack.Screen name="screens/Teacher_MainPage" />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Provider store={store}>
+        <PersistGate loading={<LoadingScreen />} persistor={persistor}>
+          <ThemeProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="index" />
+              <Stack.Screen name="screens/select/index" />
+              <Stack.Screen name="screens/teacher_login/index" />
+              <Stack.Screen name="screens/student_login/index" />
+              <Stack.Screen name="screens/Teacher_MainPage" />
 
-      </Stack>
-    </ThemeProvider>
-    </PersistGate>
-    </Provider>
+            </Stack>
+          </ThemeProvider>
+        </PersistGate>
+      </Provider>
+    </GestureHandlerRootView>
   );
 
   // 리덕스 로딩용 화면 
-function LoadingScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <ActivityIndicator size="large" color="#0000ff" />
-    </View>
-  );
-}
+  function LoadingScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
 }
