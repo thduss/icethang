418d0c96b3634e1f8da6090650f607cf58fe3011
종효련 <jhl2_014@naver.com>
2026-01-28 17:16:46 +0900

    [S14P11E204-147] feature : 시간 측정 함수 구현

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -4,21 +4,51 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import { store, persistor } from './store/stores';
-import { Provider } from 'react-redux';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { ActivityIndicator, View } from 'react-native';
+import { ActivityIndicator, View, AppState } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
-
+import { checkAndConvertTime, updatePeriodOnServer } from './store/slices/timeSlice';
+import { RootState, AppDispatch } from './store/stores';
 
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+function TimeManager() {
+  const dispatch = useDispatch<AppDispatch>();
+  const currentPeriod = useSelector((state: RootState) => state.time.currentPeriod);
 
+  useEffect(() => {
+    dispatch(checkAndConvertTime());
+    const timer = setInterval(() => {
+      dispatch(checkAndConvertTime());
+    }, 60000);
+
+    const subscription = AppState.addEventListener('change', (nextAppState) => {
+      if (nextAppState === 'active') {
+        dispatch(checkAndConvertTime());
+      }
+    });
+
+    return () => {
+      clearInterval(timer);
+      subscription.remove();
+    };
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (currentPeriod > 0) {
+      dispatch(updatePeriodOnServer(currentPeriod));
+    }
+  }, [currentPeriod, dispatch]);
+
+  return null;
+}
+
+export default function RootLayout() {
   useEffect(() => {
     const timer = setTimeout(async () => {
       await SplashScreen.hideAsync();
     }, 500);
-
     return () => clearTimeout(timer);
   }, []);
 
@@ -26,6 +56,7 @@ export default function RootLayout() {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Provider store={store}>
         <PersistGate loading={<LoadingScreen />} persistor={persistor}>
+          <TimeManager /> 
           <ThemeProvider>
             <Stack screenOptions={{ headerShown: false }}>
               <Stack.Screen name="index" />
@@ -33,20 +64,19 @@ export default function RootLayout() {
               <Stack.Screen name="screens/teacher_login/index" />
               <Stack.Screen name="screens/student_login/index" />
               <Stack.Screen name="screens/Teacher_MainPage" />
-
             </Stack>
           </ThemeProvider>
         </PersistGate>
       </Provider>
     </GestureHandlerRootView>
   );
+}
 
-  // 리덕스 로딩용 화면 
-  function LoadingScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" color="#0000ff" />
-      </View>
-    );
-  }
+// 리덕스 로딩용 화면 
+function LoadingScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator size="large" color="#0000ff" />
+    </View>
+  );
 }
