c07d56ec04bf4827f1fde4fa77ce7a1099ce9f34
김상지 <139340193+theundergroundt@users.noreply.github.com>
2026-01-29 12:01:59 +0900

    [S14P11E204-158] feature : 모드 변경 로직 추가 구현

--- backend/src/main/java/com/ssafy/icethang/domain/monitoring/dto/AlertType.java
+++ backend/src/main/java/com/ssafy/icethang/domain/monitoring/dto/AlertType.java
@@ -4,7 +4,4 @@ public enum AlertType {
     FOCUS,
     UNFOCUS,
     AWAY
-    /*OUT_OF_SEAT,     // 자리 이탈
-    COMPLETELY_OUT,  // 완전 이탈
-    SLEEPING         // 졸음*/
 }
---
+++ backend/src/main/java/com/ssafy/icethang/domain/monitoring/dto/ModeChangeRequest.java
@@ -0,0 +1,13 @@
+package com.ssafy.icethang.domain.monitoring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ModeChangeRequest {
+    private Long classId;  // 몇 반인지
+    private String mode;   // 변경할 모드 ( "NORMAL", "DIGITAL")
+}
